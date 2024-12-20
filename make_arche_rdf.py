bf93a8dbb7eff69e8adb7f9fcfc0a40cddd81ec1
csae8092 <p.andorfer@gmail.com>
2024-12-20 12:00:40 +0100

    first shot to get license mess right

--- make_arche_rdf.py
+++ make_arche_rdf.py
@@ -22,17 +22,15 @@ COL_URIS = set()
 ihb_owner = """
 @prefix acdh: <https://vocabs.acdh.oeaw.ac.at/schema#> .
 
-<https://foo/bar> acdh:hasLicensor <https://d-nb.info/gnd/1202798799> ;
-    acdh:hasOwner <https://d-nb.info/gnd/1202798799> ;
-    acdh:hasRightsHolder <https://d-nb.info/gnd/1202798799> .
+<https://foo/bar> acdh:hasLicensor <https://id.acdh.oeaw.ac.at/oeawihb> ;
+    acdh:hasOwner <https://id.acdh.oeaw.ac.at/oeawihb> ;
+    acdh:hasLicense <https://vocabs.acdh.oeaw.ac.at/archelicenses/cc-by-4-0> ;
+    acdh:hasRightsHolder <https://id.acdh.oeaw.ac.at/oeaw> .
 """
 ihb_owner_graph = Graph().parse(data=ihb_owner)
 
-for p, o in ihb_owner_graph.predicate_objects():
-    g.add((TOP_COL_URI, p, o))
 
-
-files = sorted(glob.glob("data/editions/*.xml"))[:5]
+files = sorted(glob.glob("data/editions/*.xml"))[100:105]
 for x in tqdm(files):
     doc = TeiReader(x)
     cur_col_id = os.path.split(x)[-1].replace(".xml", "")
@@ -48,6 +46,26 @@ for x in tqdm(files):
     # TEI/XML Document
     cur_doc_uri = URIRef(f"{TOP_COL_URI}/{cur_doc_id}")
     g.add((cur_doc_uri, RDF.type, ACDH["Resource"]))
+    g.add(
+        (cur_doc_uri, ACDH["hasCreator"], URIRef("https://id.acdh.oeaw.ac.at/kkeller"))
+    )
+    g.add(
+        (cur_doc_uri, ACDH["hasCreator"], URIRef("https://id.acdh.oeaw.ac.at/ipeper"))
+    )
+    g.add(
+        (
+            cur_doc_uri,
+            ACDH["hasCreator"],
+            URIRef("https://id.acdh.oeaw.ac.at/aspitzbart"),
+        )
+    )
+    g.add(
+        (
+            cur_doc_uri,
+            ACDH["hasContributor"],
+            URIRef("https://d-nb.info/gnd/1043833846"),
+        )
+    )
     g.add((cur_doc_uri, ACDH["isPartOf"], cur_col_uri))
     g.add(
         (
@@ -196,24 +214,189 @@ for x in tqdm(files):
     )
 
     # images
-    try:
-        repo = doc.any_xpath(".//tei:repository/text()")[0]
-    except IndexError:
-        owner_uri = URIRef("https://d-nb.info/gnd/2005486-5")
-        repo = "whatever"
-
-    if "Karlsruhe" in repo:
-        owner_uri = URIRef("https://d-nb.info/gnd/1014584-9")
-    elif "korrespondenz" in x:
-        owner_uri = URIRef("https://d-nb.info/gnd/4655277-7")
-    else:
-        owner_uri = URIRef("https://d-nb.info/gnd/2005486-5")
 
     for i, image in enumerate(
         doc.any_xpath(".//tei:facsimile/tei:surface/tei:graphic[@url]"), start=1
     ):
         cur_image_id = f"{cur_col_id}___{i:04}.jpg"
         cur_image_uri = URIRef(f"{TOP_COL_URI}/{cur_image_id}")
+
+        try:
+            repo = doc.any_xpath(".//tei:repository/text()")[0]
+        except IndexError:
+            owner_uri = URIRef("https://d-nb.info/gnd/2005486-5")
+            repo = "whatever"
+
+        if "Karlsruhe" in repo:
+            owner_uri = URIRef("https://d-nb.info/gnd/1014584-9")
+            g.add(
+                (
+                    cur_image_uri,
+                    ACDH["hasLicense"],
+                    URIRef("https://vocabs.acdh.oeaw.ac.at/archelicenses/noc-cr"),
+                )
+            )
+            g.add(
+                (
+                    cur_image_uri,
+                    ACDH["hasLicense"],
+                    URIRef("https://vocabs.acdh.oeaw.ac.at/archelicenses/noc-cr"),
+                )
+            )
+            g.add(
+                (
+                    cur_image_uri,
+                    ACDH["hasRightsHolder"],
+                    URIRef("https://d-nb.info/gnd/1014584-9"),
+                )
+            )
+            g.add(
+                (
+                    cur_image_uri,
+                    ACDH["hasDigitisingAgent"],
+                    URIRef("https://d-nb.info/gnd/1014584-9"),
+                )
+            )
+            g.add(
+                (
+                    cur_col_uri,
+                    ACDH["hasRightsHolder"],
+                    URIRef("https://d-nb.info/gnd/1014584-9"),
+                )
+            )
+            g.add(
+                (
+                    cur_image_uri,
+                    ACDH["hasOwner"],
+                    URIRef("https://d-nb.info/gnd/1014584-9"),
+                )
+            )
+            g.add(
+                (
+                    cur_col_uri,
+                    ACDH["hasOwner"],
+                    URIRef("https://d-nb.info/gnd/1014584-9"),
+                )
+            )
+            g.add(
+                (
+                    cur_image_uri,
+                    ACDH["hasLicensor"],
+                    URIRef("https://d-nb.info/gnd/1014584-9"),
+                )
+            )
+            g.add(
+                (
+                    cur_col_uri,
+                    ACDH["hasLicensor"],
+                    URIRef("https://d-nb.info/gnd/1014584-9"),
+                )
+            )
+            g.add(
+                (
+                    cur_image_uri,
+                    ACDH["hasRightsInformation"],
+                    Literal(
+                        "Jedwede Nachnutzung der Bilddigitalisate bedarf einer schriftlichen Genehmigung seitens des Generallandesarchivs Karlsruhe.",  # noqa
+                        lang="de",
+                    ),
+                )
+            )
+            g.add(
+                (
+                    cur_image_uri,
+                    ACDH["hasRightsInformation"],
+                    Literal(
+                        "Any re-use of the digitised images requires written permission from the Generallandesarchiv Karlsruhe.",  # noqa
+                        lang="en",
+                    ),
+                )
+            )
+            g.add(
+                (
+                    cur_col_uri,
+                    ACDH["hasRightsInformation"],
+                    Literal(
+                        "Jedwede Nachnutzung der Bilddigitalisate bedarf einer schriftlichen Genehmigung seitens des Generallandesarchivs Karlsruhe.",  # noqa
+                        lang="de",
+                    ),
+                )
+            )
+            g.add(
+                (
+                    cur_col_uri,
+                    ACDH["hasRightsInformation"],
+                    Literal(
+                        "Any re-use of the digitised images requires written permission from the Generallandesarchiv Karlsruhe.",  # noqa
+                        lang="en",
+                    ),
+                )
+            )
+        else:
+            owner_uri = URIRef("https://d-nb.info/gnd/2005486-5")
+            g.add(
+                (
+                    cur_image_uri,
+                    ACDH["hasLicense"],
+                    URIRef("https://vocabs.acdh.oeaw.ac.at/archelicenses/cc0-1-0"),
+                )
+            )
+            g.add(
+                (
+                    cur_col_uri,
+                    ACDH["hasLicense"],
+                    URIRef("https://vocabs.acdh.oeaw.ac.at/archelicenses/cc0-1-0"),
+                )
+            )
+            g.add(
+                (
+                    cur_image_uri,
+                    ACDH["hasLicensor"],
+                    URIRef("https://d-nb.info/gnd/2005486-5"),
+                )
+            )
+            g.add(
+                (
+                    cur_col_uri,
+                    ACDH["hasLicensor"],
+                    URIRef("https://d-nb.info/gnd/2005486-5"),
+                )
+            )
+            g.add(
+                (
+                    cur_image_uri,
+                    ACDH["hasOwner"],
+                    URIRef("https://d-nb.info/gnd/2005486-5"),
+                )
+            )
+            g.add(
+                (
+                    cur_col_uri,
+                    ACDH["hasOwner"],
+                    URIRef("https://d-nb.info/gnd/2005486-5"),
+                )
+            )
+            g.add(
+                (
+                    cur_image_uri,
+                    ACDH["hasRightsHolder"],
+                    URIRef("https://d-nb.info/gnd/2005486-5"),
+                )
+            )
+            g.add(
+                (
+                    cur_col_uri,
+                    ACDH["hasRightsHolder"],
+                    URIRef("https://d-nb.info/gnd/2005486-5"),
+                )
+            )
+            g.add(
+                (
+                    cur_image_uri,
+                    ACDH["hasDigitisingAgent"],
+                    URIRef("https://d-nb.info/gnd/2005486-5"),
+                )
+            )
         g.add((cur_image_uri, RDF.type, ACDH["Resource"]))
         g.add((cur_image_uri, ACDH["isPartOf"], cur_col_uri))
         g.add(
@@ -224,13 +407,6 @@ for x in tqdm(files):
             )
         )
         g.add((cur_image_uri, ACDH["hasTitle"], Literal(f"{cur_image_id}", lang="und")))
-        g.add(
-            (
-                cur_image_uri,
-                ACDH["hasLicense"],
-                URIRef("https://vocabs.acdh.oeaw.ac.at/archelicenses/noc-oklr"),
-            )
-        )
         g.add((cur_image_uri, ACDH["hasLicensor"], owner_uri))
         g.add((cur_image_uri, ACDH["hasOwner"], owner_uri))
         g.add((cur_image_uri, ACDH["hasRightsHolder"], owner_uri))
@@ -254,6 +430,34 @@ for y in ["indices", "meta"]:
         doc = TeiReader(x)
         cur_doc_id = os.path.split(x)[-1]
         cur_doc_uri = URIRef(f"{TOP_COL_URI}/{cur_doc_id}")
+        g.add(
+            (
+                cur_doc_uri,
+                ACDH["hasCreator"],
+                URIRef("https://id.acdh.oeaw.ac.at/kkeller"),
+            )
+        )
+        g.add(
+            (
+                cur_doc_uri,
+                ACDH["hasCreator"],
+                URIRef("https://id.acdh.oeaw.ac.at/ipeper"),
+            )
+        )
+        g.add(
+            (
+                cur_doc_uri,
+                ACDH["hasCreator"],
+                URIRef("https://id.acdh.oeaw.ac.at/aspitzbart"),
+            )
+        )
+        g.add(
+            (
+                cur_doc_uri,
+                ACDH["hasContributor"],
+                URIRef("https://d-nb.info/gnd/1043833846"),
+            )
+        )
         g.add((cur_doc_uri, RDF.type, ACDH["Resource"]))
         g.add((cur_doc_uri, ACDH["isPartOf"], URIRef(f"{TOP_COL_URI}/{y}")))
         g.add(
@@ -324,4 +528,6 @@ for x in files_to_ingest:
 
 
 print(f"copy title image into {to_ingest}")
-shutil.copy("./html/bio-pics/emt_person_id__9.jpg", os.path.join(to_ingest, "title-image.jpg"))
+shutil.copy(
+    "./html/bio-pics/emt_person_id__9.jpg", os.path.join(to_ingest, "title-image.jpg")
+)
